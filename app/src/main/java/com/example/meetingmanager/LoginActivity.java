0a3c7f254beebc61cba1fc35d246a57621c36d1d
HunterLC <lc411887055@gmail.com>
2019-01-24 18:01:04 +0800

    face unlock

--- app/src/main/java/com/example/meetingmanager/LoginActivity.java
+++ app/src/main/java/com/example/meetingmanager/LoginActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 public class LoginActivity extends AppCompatActivity
 {
 
     private TransitionView mAnimView;
+    Button faceLogin ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,6 +21,14 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
 
         mAnimView = findViewById(R.id.ani_view);
+        faceLogin = (Button)findViewById(R.id.btn_registered_facelogin);
+        faceLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,TestActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mAnimView.setOnAnimationEndListener(new TransitionView.OnAnimationEndListener()
         {
