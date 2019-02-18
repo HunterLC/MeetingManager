eeeb59a0319de9b9ab8f83952f9df3d04555cd85
HunterLC <lc411887055@gmail.com>
2019-02-18 13:10:13 +0800

    消息渠道

--- app/src/main/java/com/example/meetingmanager/RoomSelectionActivity.java
+++ app/src/main/java/com/example/meetingmanager/RoomSelectionActivity.java
@@ -1,12 +1,18 @@
 package com.example.meetingmanager;
 
 import android.app.DatePickerDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -110,7 +116,37 @@ public class RoomSelectionActivity extends AppCompatActivity{
         commitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(RoomSelectionActivity.this,"你已经成功预约了本次会议",Toast.LENGTH_SHORT).show();
+                NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                // 通知渠道的id
+                String id = "my_channel_01";
+                // 用户可以看到的通知渠道的名字.
+                CharSequence name = getString(R.string.app_name);
+                // 用户可以看到的通知渠道的描述
+                String description = getString(R.string.app_name);
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+                // 配置通知渠道的属性
+                mChannel.setDescription(description);
+                // 设置通知出现时的闪灯（如果 android 设备支持的话）
+                mChannel.enableLights(true);
+                mChannel.setLightColor(Color.RED);
+                // 设置通知出现时的震动（如果 android 设备支持的话）
+                mChannel.enableVibration(true);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+               //最后在notificationmanager中创建该通知渠道
+                manager.createNotificationChannel(mChannel);
+                String CHANNEL_ID = "my_channel_01";
+                Notification notification = new Notification.Builder(RoomSelectionActivity.this)
+                        .setContentText("This is a test")
+                        .setContentTitle("This is a title")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_icon)
+                        .setTicker("you have a new information,check it now?")
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_icon))
+                        .setChannelId(CHANNEL_ID)
+                        .build();
+                manager.notify(1,notification);
+               Toast.makeText(RoomSelectionActivity.this,"你已经成功预约了本次会议",Toast.LENGTH_SHORT).show();
             }
         });
 
